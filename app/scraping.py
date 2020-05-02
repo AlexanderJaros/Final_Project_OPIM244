c23daffee301aecbecffb45e7ba53273b0223879
AlexanderJaros <59668912+AlexanderJaros@users.noreply.github.com>
2020-05-01 23:00:11 -0400

    figuring out display

--- app/scraping.py
+++ app/scraping.py
@@ -17,15 +17,18 @@ import requests
 #sex = form.getvalue('sex')
 
 
-#name = input("Please enter your baby's first name: ")
-#sex = input("Please enter your baby's sex as either 'M' or 'F': ")
+name = input("Please enter your baby's first name: ")
+sex = input("Please enter your baby's sex as either 'M' or 'F': ")
 
+def ssa_scrape(name, sex):
+    request_url = "https://www.ssa.gov/cgi-bin/babyname.cgi"
+    params = {"name": name, "sex": sex}
+    response = requests.post(request_url, params)
+    print(response.status_code)
+    return print(response.text)
 
-request_url = "https://www.ssa.gov/cgi-bin/babyname.cgi"
-params = {"name": name, "sex": sex}
-response = requests.post(request_url, params)
-print(response.status_code)
-print(response.text)
+ssa_scrape(name, sex)
+
 #breakpoint()
 # todo:
 # soup = BeautifulSoup(response.text)
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -2,6 +2,7 @@
 
 from flask import Blueprint, render_template, redirect, flash, request 
 import requests
+from web_app.routes.scraping import ssa_scrape 
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -10,27 +11,26 @@ def index():
     print("VISITED THE HOME PAGE")
     return render_template("home.html")
 
-@home_routes.route("/nameresult")
-def nameresult():
-    print("VISITED THE NAME RESULT PAGE")
-    #return "About Me (TODO)"
-    return render_template("nameresult.html")
-
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
     #return "About Me (TODO)"
-    return render_template("about.html")
+    return render_template("about.html") 
+
+#@home_routes.route("/nameresult")
+#def nameresult():
+#    print("VISITED THE NAME RESULT PAGE")
+#    #return "About Me (TODO)"
+#    return render_template("nameresult.html")
 
 @home_routes.route("/nameresult", methods=["POST"]) #https://www.youtube.com/watch?v=AEM8_4NBU04
 def passvalue():
     name = request.form["name"]
     sex = request.form["sex"]
-    request_url = "https://www.ssa.gov/cgi-bin/babyname.cgi"
-    params = {"name": name, "sex": sex}
-    response = requests.post(request_url, params)
-    print(response.status_code)
-    output = print(response.text)
+    #render_template("nameresult.html", name=name, sex=sex)
+    output = ssa_scrape(name, sex)
+    #output = response.text
     #print(name)
     #print (sex)
-    return render_template("nameresult.html", output=output, name=name, sex=sex)
+    return render_template("nameresult.html", output = output, name=name, sex=sex)
+    
