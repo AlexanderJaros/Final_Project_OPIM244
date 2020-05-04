e3a7c6b23a4a0e599fcb02c37037467ce269505b
AlexanderJaros <59668912+AlexanderJaros@users.noreply.github.com>
2020-05-04 13:45:53 -0400

    Playing around a bit

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -6,22 +6,30 @@ from web_app.routes.scraping import ssa_scrape
 
 home_routes = Blueprint("home_routes", __name__)
 
+@home_routes.route("/nameresult")
+def nameresult():
+#   print("VISITED THE NAME RESULT PAGE")
+    return render_template("nameresult.html")
+
 @home_routes.route("/")
 def index():
     print("VISITED THE HOME PAGE")
     return render_template("home.html")
+def passvalue():
+    name = request.form["name"]
+    sex = request.form["sex"]
+    #render_template("nameresult.html", name=name, sex=sex)
+    output = ssa_scrape(name, sex)
+    #output = response.text
+    #print(name)
+    #print (sex)
+    return redirect("index.html", output = output, name=name, sex=sex)
 
 @home_routes.route("/about")
 def about():
     print("VISITED THE ABOUT PAGE")
     #return "About Me (TODO)"
     return render_template("about.html") 
-
-#@home_routes.route("/nameresult")
-#def nameresult():
-#    print("VISITED THE NAME RESULT PAGE")
-#    #return "About Me (TODO)"
-#    return render_template("nameresult.html")
 
 @home_routes.route("/nameresult", methods=["POST"]) #https://www.youtube.com/watch?v=AEM8_4NBU04
 def passvalue():
